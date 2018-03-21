98b066dd93bec3cd215065a716884632f0bbf343
Kry127 <kry127@yandex.ru>
2018-03-21 12:38:07 +0300

    Revision made for EPAM test page #1

--- src/test/java/com/spbstu/selenide/SelenideTest.java
+++ src/test/java/com/spbstu/selenide/SelenideTest.java
@@ -1,6 +1,7 @@
 package com.spbstu.selenide;
 
 import com.spbstu.selenium.BaseSeleniumTest;
+import lombok.AllArgsConstructor;
 import org.bouncycastle.eac.EACCertificateBuilder;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -12,11 +13,26 @@ public class SelenideTest extends BaseSelenideTest {
         EpamTestSiteSelenide.init();
     }
 
+    // поставить плагин Lombok, чтобы заработал декоратор
+    // dependency недостаточно. Он позволяет убирать констркутор
+    // TODO -- подключить плагин LOMBOK
+    //@AllArgsConstructor
+    enum HOME_PAGE_DATA {
+        LOGIN("epam"), PASSWORD("1234"), USER_NAME("PITER_CHAILOVSKI");
+
+        String value;
+
+        // TODO -- убрать это с помощью LOMBOK
+        HOME_PAGE_DATA(String value) {
+            this.value = value;
+        }
+    }
+
     @Test
     public void selenideTest() {
         EpamTestSiteSelenide.homePageSelenide.open();
-        EpamTestSiteSelenide.homePageSelenide.login("epam", "1234");
-        EpamTestSiteSelenide.homePageSelenide.checkUserLogIn("PITER CHAILOVSKI");
+        EpamTestSiteSelenide.homePageSelenide.login(HOME_PAGE_DATA.LOGIN.toString(), HOME_PAGE_DATA.PASSWORD.toString());
+        EpamTestSiteSelenide.homePageSelenide.checkUserLogIn(HOME_PAGE_DATA.USER_NAME.toString());
     }
 
 }
