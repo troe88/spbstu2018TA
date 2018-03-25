4c8c81e78fa1ad43f3273b71c9bc57bf4fc65b27
Kry127 <kry127@yandex.ru>
2018-03-25 20:24:23 +0300

    Enum of HW1 refactored to use lombok

--- src/test/java/com/spbstu/hw1/EpamSiteTestClass.java
+++ src/test/java/com/spbstu/hw1/EpamSiteTestClass.java
@@ -28,7 +28,7 @@ public class EpamSiteTestClass {
 
     @Test
     public void epamLoginSimpleTest() throws InterruptedException {
-        driver.navigate().to(EpamSiteTestEnum.EPAM_TEST_LINK);
+        driver.navigate().to(EPAM_TEST_LINK.getValue());
         // не ждём одну секунду, а делаем хитрее -- ждём, пока браузер загрузится
         // Ссылка на C# код, но полиморфизм сохранён:
         // https://stackoverflow.com/questions/5868439/wait-for-page-load-in-selenium
@@ -38,47 +38,47 @@ public class EpamSiteTestClass {
         driver.manage().window().maximize();
 
         // Проверяем, что заголовок страницы совпадает с ожидаемым
-        Assert.assertEquals(driver.getTitle(), INDEX_PAGE,
-                String.format("Actual: %s but expected: %s", driver.getTitle(), INDEX_PAGE));
+        Assert.assertEquals(driver.getTitle(), INDEX_PAGE.getValue(),
+                String.format("Actual: %s but expected: %s", driver.getTitle(), INDEX_PAGE.getValue()));
 
         // выполняем клик по иконке пользователя
-        WebElement element = driver.findElement(By.cssSelector(DROPDOWN_CSS_SELECTOR));
+        WebElement element = driver.findElement(By.cssSelector(DROPDOWN_CSS_SELECTOR.getValue()));
         element.click();
         // заполняем поле логин
-        element = driver.findElement(By.cssSelector(LOGIN_CSS_SELECTOR));
-        element.sendKeys(LOGIN);
+        element = driver.findElement(By.cssSelector(LOGIN_CSS_SELECTOR.getValue()));
+        element.sendKeys(LOGIN.getValue());
         // заполняем поле пароля и нажимаем клавишу Enter
-        element = driver.findElement(By.cssSelector(PASSWORD_CSS_SELECTOR));
-        element.sendKeys(PASSWORD + Keys.ENTER);
+        element = driver.findElement(By.cssSelector(PASSWORD_CSS_SELECTOR.getValue()));
+        element.sendKeys(PASSWORD.getValue() + Keys.ENTER);
 
         // проверяем, что имя пользователя совпадает с ожидаемым
-        element = driver.findElement(By.cssSelector(USERNAME_CSS_SELECTOR));
+        element = driver.findElement(By.cssSelector(USERNAME_CSS_SELECTOR.getValue()));
         element.getText();
-        sa.assertEquals(element.getText(), USER_FULL_DISPLAY_NAME
-                ,String.format(DIAGNOSTIC_STRING, driver.getTitle(), INDEX_PAGE));
+        sa.assertEquals(element.getText(), USER_FULL_DISPLAY_NAME.getValue()
+                ,String.format(DIAGNOSTIC_STRING.getValue(), driver.getTitle(), INDEX_PAGE.getValue()));
 
         // получаем массив картинок, и смотрим, что их количество соответствует ожидаемому
-        sa.assertEquals(driver.getTitle(), INDEX_PAGE
-                ,String.format(DIAGNOSTIC_STRING, driver.getTitle(), INDEX_PAGE));
-        int sz = driver.findElements(By.cssSelector(ICONS_CSS_SELECTOR)).size();
+        sa.assertEquals(driver.getTitle(), INDEX_PAGE.getValue()
+                ,String.format(DIAGNOSTIC_STRING.getValue(), driver.getTitle(), INDEX_PAGE.getValue()));
+        int sz = driver.findElements(By.cssSelector(ICONS_CSS_SELECTOR.getValue())).size();
         sa.assertEquals(sz, 4
-                ,String.format(DIAGNOSTIC_STRING, sz, 4));
+                ,String.format(DIAGNOSTIC_STRING.getValue(), sz, 4));
 
         // проверяем совпадения текста под фотографиями
-        List<String> assertStrings = Arrays.asList(WISH1, WISH2, WISH3, WISH4);
+        List<String> assertStrings = Arrays.asList(WISH1.getValue(), WISH2.getValue(), WISH3.getValue(), WISH4.getValue());
         driver.findElements(By.cssSelector(".benefit-txt"))
                 .stream().map(WebElement::getText).forEach(
                         text->sa.assertTrue(assertStrings.contains(text),
-                                WISH_NOT_FOUND_DIAGNOSTIC_STRING + text)
+                                WISH_NOT_FOUND_DIAGNOSTIC_STRING.getValue() + text)
                 );
 
         // проверяем совпадения заголовка текста в теге h3 с ожидаемым
-        sa.assertEquals(driver.findElement(By.cssSelector(TITLE_CSS_SELECTOR)).getText(),
-                EPAM_FRAMEWORK_WISHES, DIAGNOSTIC_STRING);
+        sa.assertEquals(driver.findElement(By.cssSelector(TITLE_CSS_SELECTOR.getValue())).getText(),
+                EPAM_FRAMEWORK_WISHES.getValue(), DIAGNOSTIC_STRING.getValue());
 
         // проверяем основное содержимое страницы на совпадение с ожидаемым
-        sa.assertEquals(driver.findElement(By.cssSelector( ANNOTATION_CSS_SELECTOR)).getText(),
-                LOREM_IPSUM_DOLOR, DIAGNOSTIC_STRING);
+        sa.assertEquals(driver.findElement(By.cssSelector( ANNOTATION_CSS_SELECTOR.getValue())).getText(),
+                LOREM_IPSUM_DOLOR.getValue(), DIAGNOSTIC_STRING.getValue());
 
         // выполняем SoftAssert
         sa.assertAll();
--- src/test/java/com/spbstu/hw1/EpamSiteTestEnum.java
+++ src/test/java/com/spbstu/hw1/EpamSiteTestEnum.java
@@ -1,25 +1,31 @@
 package com.spbstu.hw1;
 
-enum EpamSiteTestEnum {;
+import lombok.AllArgsConstructor;
 
-    static final String EPAM_TEST_LINK = "https://jdi-framework.github.io/tests";
-    static final String INDEX_PAGE = "Index Page";
-    static final String LOGIN = "epam";
-    static final String PASSWORD = "1234";
-    static final String DROPDOWN_CSS_SELECTOR = ".profile-photo";
-    static final String LOGIN_CSS_SELECTOR = "#Login";
-    static final String PASSWORD_CSS_SELECTOR = "#Password";
-    static final String USERNAME_CSS_SELECTOR = ".profile-photo > span";
-    static final String USER_FULL_DISPLAY_NAME = "PITER CHAILOVSKII";
-    static final String DIAGNOSTIC_STRING = "Actual: %s but expected: %s";
-    static final String ANNOTATION_CSS_SELECTOR = ".main-content > p";
-    static final String TITLE_CSS_SELECTOR = ".main-content > h3";
-    static final String EPAM_FRAMEWORK_WISHES = "EPAM FRAMEWORK WISHES…";
-    static final String LOREM_IPSUM_DOLOR = "LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISICING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO CONSEQUAT DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.";
-    static final String WISH1 = "To include good practices\nand ideas from successful\nEPAM projec";
-    static final String WISH2 = "To be flexible and\ncustomizable";
-    static final String WISH3 = "To be multiplatform";
-    static final String WISH4 = "Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…";
-    static final String WISH_NOT_FOUND_DIAGNOSTIC_STRING = "Text not found in the acceptable wishlist of framework ";
-    static final String ICONS_CSS_SELECTOR = ".benefit-icon";
+@AllArgsConstructor
+@lombok.Getter
+enum EpamSiteTestEnum {
+
+    EPAM_TEST_LINK("https://jdi-framework.github.io/tests"),
+    INDEX_PAGE("Index Page"),
+    LOGIN("epam"),
+    PASSWORD("1234"),
+    DROPDOWN_CSS_SELECTOR(".profile-photo"),
+    LOGIN_CSS_SELECTOR("#Login"),
+    PASSWORD_CSS_SELECTOR("#Password"),
+    USERNAME_CSS_SELECTOR(".profile-photo > span"),
+    USER_FULL_DISPLAY_NAME("PITER CHAILOVSKII"),
+    DIAGNOSTIC_STRING("Actual: %s but expected: %s"),
+    ANNOTATION_CSS_SELECTOR(".main-content > p"),
+    TITLE_CSS_SELECTOR(".main-content > h3"),
+    EPAM_FRAMEWORK_WISHES("EPAM FRAMEWORK WISHES…"),
+    LOREM_IPSUM_DOLOR("LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISICING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO CONSEQUAT DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR."),
+    WISH1("To include good practices\nand ideas from successful\nEPAM projec"),
+    WISH2("To be flexible and\ncustomizable"),
+    WISH3("To be multiplatform"),
+    WISH4("Already have good base\n(about 20 internal and\nsome external projects),\nwish to get more…"),
+    WISH_NOT_FOUND_DIAGNOSTIC_STRING("Text not found in the acceptable wishlist of framework "),
+    ICONS_CSS_SELECTOR(".benefit-icon");
+
+    String value;
 }
